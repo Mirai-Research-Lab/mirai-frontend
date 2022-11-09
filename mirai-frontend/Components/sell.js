dcefdf2566ff6978ca89f7fd0eb16b3c0b02b8c2
Prasun <prasunmondal60@gmail.com>
2022-11-09 22:25:26 +0530

    router set for sell page

--- mirai-frontend/Components/sell.js
+++ mirai-frontend/Components/sell.js
@@ -1,9 +1,78 @@
 import React from 'react'
+import Image from "next/image";
+import nft from "../public/nft.jpg"
+import nft2 from "../public/nft2.jpg"
+import nft3 from "../public/nft3.jpg"
+import nft4 from "../public/nft4.webp"
 
-function sell() {
+function Sell() {
   return (
-    <div>sell</div>
+    <>
+      <div className="marketplace-container ">
+        <div className="marketplace-heading">
+          <h1>MARKETPLACE</h1>
+          <span>Buy NFTs</span>
+        </div>
+
+        <div className="nft-cards">
+          <div className="nft-card">
+            <Image src={nft} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+          <div className="nft-card">
+            <Image src={nft3} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+          <div className="nft-card">
+            <Image src={nft2} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+          <div className="nft-card">
+            <Image src={nft4} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+          <div className="nft-card">
+            <Image src={nft2} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+          <div className="nft-card">
+            <Image src={nft3} className="nft-image" />
+            <div className="nft-card-info">
+              <div className="nft-card-info-heading">
+                <h1>Card Name</h1>
+                <span>Card Description</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default sell
+export default Sell
---
+++ mirai-frontend/pages/marketplace/sell.js
@@ -0,0 +1,16 @@
+import Sell from "../../Components/Sell.js";
+import Features from "../../Components/Option.js"
+import New from "../../Components/New.js";
+import Navbar from '../../Components/nav.js';
+
+
+function index() {
+  return (
+    <div>
+      <Navbar/>
+        <Sell/>
+    </div>
+  )
+}
+
+export default index 
