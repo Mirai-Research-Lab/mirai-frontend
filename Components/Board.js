b029816c14571ab2b281d9aeacc4dcf90f0006b9
Debajyoti <debajyotidhar111@gmail.com>
2022-11-14 19:52:15 +0530

    Connected data of leaderboard

--- Components/Board.js
+++ Components/Board.js
@@ -1,56 +1,58 @@
-import React, { useState } from 'react'
-import Profiles from './profiles';
-import { Leaderboard } from './database';
-import styles from "../styles/leaderboard.module.css"
+import React, { useState } from "react";
+import Profiles from "./profiles";
+import { Leaderboard } from "./database";
+import styles from "../styles/leaderboard.module.css";
 import TOP3 from "./top3.js";
 
-export default function Board() {
-
-    const [period, setPeriod] = useState(0);
+export default function Board({ players }) {
+  const [period, setPeriod] = useState(0);
 
   const handleClick = (e) => {
-     
     setPeriod(e.target.dataset.id);
-  }
+  };
 
   return (
     <div className={styles.board}>
-        
-        <h1 className={styles.leaderboard}>Leaderboard</h1>
+      <h1 className={styles.leaderboard}>Leaderboard</h1>
 
-        <div className={styles.duration}>
-            <button onClick={handleClick} data-id='7'>7 Days</button>
-            <button onClick={handleClick} data-id='30'>30 Days</button>
-            <button onClick={handleClick} data-id='0'>Today</button>
-        </div>
-        <TOP3 Leaderboard={between(Leaderboard, period)}/>
-        <Profiles Leaderboard={between(Leaderboard, period)}></Profiles>
-        <div className={styles.blob5}></div>
-
+      <div className={styles.duration}>
+        <button onClick={handleClick} data-id="7">
+          7 Days
+        </button>
+        <button onClick={handleClick} data-id="30">
+          30 Days
+        </button>
+        <button onClick={handleClick} data-id="0">
+          Today
+        </button>
+      </div>
+      <TOP3 Leaderboard={between(Leaderboard, period)} players={players} />
+      <Profiles
+        Leaderboard={between(Leaderboard, period)}
+        players={players}
+      ></Profiles>
+      <div className={styles.blob5}></div>
     </div>
-  )
+  );
 }
 
+function between(data, between) {
+  const today = new Date();
+  const previous = new Date(today);
+  previous.setDate(previous.getDate() - (between + 1));
 
+  let filter = data.filter((val) => {
+    let userDate = new Date(val.dt);
+    if (between == 0) return val;
+    return previous <= userDate && today >= userDate;
+  });
 
-function between(data, between){
-    const today = new Date();
-    const previous = new Date(today);
-    previous.setDate(previous.getDate() - (between + 1));
-
-    let filter = data.filter(val => {
-        let userDate = new Date(val.dt);
-        if (between == 0) return val;
-        return previous <= userDate && today >= userDate;
-    })
-
-    // sort with asending order
-    return filter.sort((a, b) => {
-        if ( a.score === b.score){
-            return b.score - a.score;
-        } else{
-            return b.score - a.score;
-        }
-    })
-
+  // sort with asending order
+  return filter.sort((a, b) => {
+    if (a.score === b.score) {
+      return b.score - a.score;
+    } else {
+      return b.score - a.score;
+    }
+  });
 }
--- Components/profiles.js
+++ Components/profiles.js
@@ -1,40 +1,31 @@
-import React from 'react'
-import styles from "../styles/leaderboard.module.css"
+import React from "react";
+import styles from "../styles/leaderboard.module.css";
 
-
-export default function profiles({ Leaderboard }) {
-  return (
-        <div id={styles.profile}>
-            {Item(Leaderboard)}
-        </div>
-  )
+export default function profiles({ Leaderboard, players }) {
+  return <div id={styles.profile}>{Item({ Leaderboard, players })}</div>;
 }
 
-function Item(data){
-    return (
+function Item({ players }) {
+  return (
+    <div className={styles.profiles}>
+      {players.map((player, index) => (
+        <div className={styles.flex} key={index}>
+          <div className={styles.item}>
+            {/* <img src={value.img} alt="" /> */}
 
-        <div className={styles.profiles}>
-            {
-                data.map((value, index) => (
-                    <div className={styles.flex} key={index}>
-                        <div className={styles.item}>
-                            <img src={value.img} alt="" />
-            
-                            <div className={styles.info}>
-                                <h3 className={styles.textdark}>{value.name}</h3>
-                                <span>Position : {index+1}</span>    
-                                <span>Address : {value.location}</span>
-                            </div>                
-                        </div>
-                        <div className={styles.item}>
-                            <span>{value.score}</span>
-                        </div>
-                    </div>
-                )
-                )
-            }
+            <div className={styles.info}>
+              <h3 className={styles.textdark}>{player.username}</h3>
+              <span>Position : {index + 1} </span>
+              <span>
+                Address : {player.funding_address || "0x000000000000000000dEaD"}
+              </span>
+            </div>
+          </div>
+          <div className={styles.item}>
+            <span>{player.highest_score}</span>
+          </div>
         </div>
-
-        
-    )
+      ))}
+    </div>
+  );
 }
