85ce05c390dce25ede73cd4b9f9b9196fe6b1d32
metal-oopa <sudip5banerjee1974@gmail.com>
2022-11-17 18:57:05 +0530

    add contract calls

--- Components/MyCard.js
+++ Components/MyCard.js
@@ -1,12 +1,13 @@
+import { useWeb3Contract, useMoralis } from "react-moralis";
 import React from "react";
 import Image from "next/image";
-import nft from "../public/nft.jpg";
-import nft2 from "../public/nft2.jpg";
-import nft3 from "../public/nft3.jpg";
-import nft4 from "../public/nft4.webp";
 import Modal from "react-modal";
-import Sell from "./sell";
 import Router from "next/router";
+
+import networkMapping from "../constants/networkMapping.json";
+import IpfsNFT from "../constants/frontEndAbiLocation/IpfsNFT.json";
+import Marketplace from "../constants/frontEndAbiLocation/Marketplace.json";
+import nft from "../public/nft.jpg";
 
 // import styles from "../styles/auth.module.css"
 
@@ -24,10 +25,34 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-// Modal.setAppElement('#yourAppElement');
 
-export default function CardDetails() {
+export default function CardDetails({ tokenId }) {
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [price, setPrice] = React.useState(0);
+  const { web3, account } = useMoralis();
+  const chainId = "5";
+
+  const { runContractFunction: approve } = useWeb3Contract({
+    abi: IpfsNFT,
+    contractAddress: networkMapping[chainId]["IpfsNFT"].slice(-1)[0],
+    functionName: "approve",
+    params: {
+      nftAddress: networkMapping[chainId]["Marketplace"].slice(-1)[0],
+      tokenId: tokenId,
+    },
+  });
+
+  const { runContractFunction: listItem } = useWeb3Contract({
+    abi: Marketplace,
+    contractAddress: networkMapping[chainId]["Marketplace"].slice(-1)[0],
+    functionName: "listItem",
+    params: {
+      nftAddress: networkMapping[chainId]["Marketplace"].slice(-1)[0],
+      tokenId: tokenId,
+      price: price,
+    },
+  });
+
   let person = {
     name: "Anshu Bokachoda",
     imageId: "1bX5QH6",
@@ -41,9 +66,31 @@ export default function CardDetails() {
   }
   function closeModaleth() {
     setIsOpen(false);
-    <Sell person={person} />;
-    Router.push("/marketplace/sell");
+    Router.reload();
   }
+
+  const handlebuy = (e) => {
+    e.preventDefault();
+
+    approve({
+      onSuccess: (result) => {
+        console.log("approve result: ", result);
+        listItem({
+          onSuccess: (result) => {
+            console.log("listItem result: ", result);
+          },
+          onError: (error) => {
+            console.log("listItem error", error);
+          },
+        });
+      },
+      onError: (error) => {
+        console.log("approve error", error);
+      },
+    });
+
+    closeModaleth();
+  };
 
   return (
     <>
@@ -54,8 +101,8 @@ export default function CardDetails() {
         </div>
 
         {/* <div className="nft-cards my-cards-div"> */}
-        <div className="nft-card">
-          <Image src={nft} className="nft-image" />
+        <div className="nft-card" onClick={(e) => openModal()}>
+          <Image src={nft} className="nft-image" alt="" />
           <div className="nft-card-info">
             <div className="nft-card-info-heading">
               <h1>Card Name</h1>
@@ -64,8 +111,12 @@ export default function CardDetails() {
                 <button onClick={openModal}>Sell</button>
                 <Modal isOpen={modalIsOpen} style={customStyles}>
                   <h2>List at Eth</h2>
-                  <input placeholder="Eth Amount"></input>
-                  <button onClick={closeModaleth}>List</button>
+                  <input
+                    placeholder="Eth Amount"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  ></input>
+                  <button onClick={(e) => handlebuy()}>List</button>
                   <button onClick={closeModal}>Close</button>
                 </Modal>
               </div>
--- Components/Profile.js
+++ Components/Profile.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { useMoralis } from "react-moralis";
+import { useMoralis, useWeb3Contract } from "react-moralis";
 import pencil from "../public/pencil.png";
 import Modal from "react-modal";
 import axios from "axios";
 import Router from "next/router";
+
+import Marketplace from "../constants/frontEndAbiLocation/Marketplace.json";
+import IpfsNFT from "../constants/frontEndAbiLocation/IpfsNFT.json";
+import networkMapping from "../constants/networkMapping.json";
 
 const customStyles = {
   overlay: {
@@ -20,8 +24,57 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-function Profile({ email, username, funding_address, img }) {
-  const { account } = useMoralis();
+function Profile({ email, username, funding_address, img, currentuser }) {
+  const { web3, account } = useMoralis();
+  const chainId = "5";
+
+  const { runContractFunction: withdrawAmount } = useWeb3Contract({
+    abi: Marketplace,
+    contractAddress: networkMapping[chainId]["Marketplace"].slice(-1)[0],
+    functionName: "withdrawAmount",
+    params: {},
+  });
+
+  const { runContractFunction: staticMint } = useWeb3Contract({
+    abi: IpfsNFT,
+    contractAddress: networkMapping[chainId]["IpfsNFT"].slice(-1)[0],
+    functionName: "staticMint",
+    params: {},
+  });
+
+  const withdraw = async () => {
+    withdrawAmount({
+      onSuccess: (txHash) => {
+        console.log(txHash);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
+  const mintnfts = async () => {
+    if (currentuser.mintCount > 0) {
+      const tokenId = staticMint({
+        onSuccess: async (result) => {
+          console.log(result);
+          await axios.post(
+            "https://mirai-backend-kappa.vercel.app/api/player/decrementmintcount",
+            {
+              withCredentials: true,
+              body: {
+                username: currentuser.username,
+              },
+            }
+          );
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+    }
+  };
+
   const isalreadyFunding = () => {
     if (account && funding_address != account)
       return (
@@ -97,12 +150,20 @@ function Profile({ email, username, funding_address, img }) {
           </div>
           <div className="fashion-studio-border pt-2">
             <span className="fashion-studio">
-              <button className="mint-nfts">Mint A NFT ( remaining)</button>
+              <button
+                className="mint-nfts"
+                onClick={mintnfts}
+                disabled={currentuser.mintCount <= 0}
+              >
+                Mint A NFT ( {currentuser.mintCount} remaining )
+              </button>
             </span>
           </div>
           <div className="fashion-studio-border pt-2">
             <span className="fashion-studio">
-              <button className="mint-nfts">Withdraw Balance</button>
+              <button className="mint-nfts" onClick={withdraw}>
+                Withdraw Balance
+              </button>
             </span>
           </div>
           {isalreadyFunding()}
